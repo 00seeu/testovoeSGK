48366371af2ee568c6c1faa22f9b685b1c897817
00seeu <120364031+00seeu@users.noreply.github.com>
2023-01-31 17:07:03 +0700

    Create 1.cs

---
+++ С#/1.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        System.Func<string, char> f =
+            s => { var c = s[0]; while (s.IndexOf(++c) >= 0) ; return c; }
+            ;
+
+        System.Console.WriteLine(f("abcdf"));  // e
+        System.Console.WriteLine(f("OQRS"));   // P
+        System.Console.WriteLine(f("xz"));     // y
+    }
+}
